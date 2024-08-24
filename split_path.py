ec92e070acbda7826a1de582998e71c9ed73d375
Glau cy <94122730+Glauu@users.noreply.github.com>
2024-08-24 12:02:43 -0300

    Update split_path.py
    Prática: criandouma situação de conflito.

--- split_path.py
+++ split_path.py
@@ -2,4 +2,3 @@ import os
 caminho_2 = '//nas.prodam/smg_cgpatri/CGPATRI_DEAPI_ACERVO_PLANTAS/GEORREFERENCIAMENTO/PLANTAS DGPI GEOREFERENCIADAS/DGPI 00.001_00\DGPI 00.001_00.shp'
 termos_1 = os.path.splitroot(caminho_2)
 print(termos_1[0])
-print(termos_1[1])
