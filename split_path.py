310e133f73955a6da5643c739e9abfc1795ca8e3
Glauu <gcelestinodasilva@gmail.com>
2024-08-24 12:10:52 -0300

    'Mudança no nome da variável'

--- split_path.py
+++ split_path.py
@@ -1,4 +1,4 @@
 import os
 caminho_2 = '//nas.prodam/smg_cgpatri/CGPATRI_DEAPI_ACERVO_PLANTAS/GEORREFERENCIAMENTO/PLANTAS DGPI GEOREFERENCIADAS/DGPI 00.001_00\DGPI 00.001_00.shp'
-termos_1 = os.path.splitroot(caminho_2)
+path_arquivo = os.path.splitroot(caminho_2)
 print(termos_1[0])
